6c99494262eafb726c50cbf1dba14884106e2e11
Falah Sultan Alizhar <falah.alizhar@mclinica.com>
2022-07-26 08:59:49 +0700

    bugfix button hyperlink

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -8,14 +8,13 @@ const CTA = () => (
       <h3>Contact me today & start the endless possibilities.</h3>
     </div>
     <div className="cta-btn">
-      <button type="button">
-        <a
-          href="https://api.whatsapp.com/send/?phone=6287773744040&text=Hi%20Falah,%20I%20have%20visited%20your%20website.%20Let%27s%20connect%21&type=phone_number"
-          target="_blank"
-          rel="noopener noreferrer"
-        > WhatsApp
-        </a>
-      </button>
+      <a
+        href="https://api.whatsapp.com/send/?phone=6287773744040&text=Hi%20Falah,%20I%20have%20visited%20your%20website.%20Let%27s%20connect%21&type=phone_number"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button type="button">WhatsApp</button>
+      </a>
     </div>
   </div>
 );
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -18,14 +18,15 @@ const Header = () => (
         />
       </h2>
       <div className="header-content__input">
-        <button type="button">
-          <a
-            href="https://drive.google.com/file/d/1HjmLxieuD8QrNWT6iwUrY8jLo7Mbv29D/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-          > Download Resume
-          </a>
-        </button>
+        <a
+          href="https://drive.google.com/file/d/1HjmLxieuD8QrNWT6iwUrY8jLo7Mbv29D/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button type="button">
+            Download Resume
+          </button>
+        </a>
       </div>
     </div>
 
